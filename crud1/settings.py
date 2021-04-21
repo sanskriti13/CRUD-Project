f42f61285f4c8d26b1c7a107cd30f3e5500bd846
sanskriti13 <63180433+sanskriti13@users.noreply.github.com>
2021-04-21 22:38:35 +0530

    Update settings.py

--- crud1/settings.py
+++ crud1/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dp+e9hcg$&-1hqjx%a#@nkyb=ql!a)d!0!&sid)cc9a%c07bt('
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
